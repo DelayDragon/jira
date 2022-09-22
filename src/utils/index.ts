a62303a5c8f5cfba13750f2ce3b271b478b2c486
DelayDragon <1130445410@qq.com>
2022-09-22 23:17:43 +0800

    update: Typescript any -> unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
 
-export const isFalsy = (value: any) => value === 0 ? false : !value
+export const isFalsy = (value: unknown) => value === 0 ? false : !value
 
 export const cleanObject = (object: Object)=>{
     const result = {...object}
     Object.keys(result).forEach(key => {
+        // @ts-ignore
         const value = result[key]
         if(isFalsy(value)){
+                    // @ts-ignore
             delete result[key]
         }
     })
@@ -19,7 +21,7 @@ export const useMount = (callback: () => void)=>{
     }, [])
 }
 //防抖hooks
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = (value: unknown, delay?: number):any => {
      const [debounceValue, setDebounceValue] = useState(value)
 
      useEffect(()=>{
