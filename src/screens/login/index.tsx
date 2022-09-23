51ff5d31bb393a93c01a6bdc6da2855dee7e7b0a
DelayDragon <1130445410@qq.com>
2022-09-23 12:47:32 +0800

    update: middleware

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,8 +1,28 @@
 import React, { FormEvent } from "react";
 
-export const LoginScreen = () => {
-    const handleSubmit = (event: FormEvent<HTMLFormElement>)=>{
+const apiUrl = process.env.REACT_APP_API_URL
 
+export const LoginScreen = () => {
+
+    const login = (param: {username:string, password: string}) => {
+        fetch(`${apiUrl}/login`,{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(param)
+        }).then(async res => {
+            if(res.ok){
+                // setList(await res.json())
+            }
+        })
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>)=>{
+        event.preventDefault()
+        const username = (event.currentTarget.elements[0] as HTMLInputElement).value
+        const password = (event.currentTarget.elements[1] as HTMLInputElement).value
+        login({username, password})
     } 
     return <form onSubmit={handleSubmit}>
         <div>
