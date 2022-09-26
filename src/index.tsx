17aa4b73f14fe1a787d008af546bcf763527f285
DelayDragon <1130445410@qq.com>
2022-09-26 22:57:41 +0800

    update: msw

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,15 @@ loadDevTools(() => {
   )
 })
 
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
