7758ebc7c7e59a86a6b121db65e9ce78fe13e01c
DelayDragon <1130445410@qq.com>
2022-09-29 10:17:11 +0800

    update : login and register screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { ProjectListScreen } from 'screens/project-list';
 import { LoginScreen } from 'screens/login';
+import { useAuth } from 'context/auth-context';
+import { AuthenticatedApp } from 'authenticated-app';
+import { UnauthenticatedApp } from 'unauthenticated-app';
 
 
 function App() {
+  const {user} = useAuth()
   return (
     <div className="App">
       {/* <ProjectListScreen></ProjectListScreen> */}
-      <LoginScreen></LoginScreen>
+      {/* <LoginScreen></LoginScreen> */}
+      {user ? <AuthenticatedApp/> : <UnauthenticatedApp/>}
     </div>
   );
 }
