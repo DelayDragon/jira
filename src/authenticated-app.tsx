df94cbef0a659a726b0b797032ee01412198a9f1
DelayDragon <1130445410@qq.com>
2022-10-03 13:11:52 +0800

    add: Row css in js组件 布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled"
+import { Row } from "component/lib"
 import { useAuth } from "context/auth-context"
 import React from "react"
 import { ProjectListScreen } from "screens/project-list"
@@ -6,8 +7,8 @@ import { ProjectListScreen } from "screens/project-list"
 export const AuthenticatedApp = () =>{
     const {logout} = useAuth()
     return <Container>
-        <Header>
-            <HeaderLeft>
+        <Header between={true}>
+            <HeaderLeft gap={true}>
                 <h3>logo</h3>
                 <h3>项目</h3>
                 <h3>用户</h3>
@@ -16,53 +17,33 @@ export const AuthenticatedApp = () =>{
                 <button onClick={logout}>登出</button>
             </HeaderRight>
         </Header>
-        <Nav>nav</Nav>
         <Main>
             <ProjectListScreen/>
         </Main>
-        <Aside>aside</Aside>
-        <Footer>footer</Footer>
     </Container>
 }
 
-
+const HeaderItem = styled.h3`
+    margin-right: 3rem;
+`
 
 const Container = styled.div`
     display: grid;
     grid-template-rows: 6rem 1fr 6rem;
-    grid-template-columns: 20rem 1fr 20rem;
-    grid-template-areas: 
-    'header header header'
-    'nav main aside'
-    'footer footer footer'
-    ;
     height: 100vh;
-    grid-gap: 5rem;
 `
-const Header = styled.header`
-    grid-area: header;
-    /* height: 6rem; */
+const Header = styled(Row)`
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
 `
-const HeaderLeft = styled.div`
-    display: flex;
-    align-items: center;
+const HeaderLeft = styled(Row)`
+    /* display: flex;
+    align-items: center; */
 `
 const HeaderRight = styled.div`
     
 `
-const Nav = styled.nav`
-    grid-area: nav;
-`
 const Main = styled.main`
-    grid-area: main;
-`
-const Aside = styled.aside`
-    grid-area: aside;
-`
-const Footer = styled.footer`
-    grid-area: footer;
 `
