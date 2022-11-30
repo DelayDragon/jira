553b64f4fc867f6a5650517efd113682cbca7c90
DelayDragon <1130445410@qq.com>
2022-11-30 11:04:19 +0800

    chore：改变setSearchParams的实现方式；

--- src/screens/project-list/util.ts
+++ src/screens/project-list/util.ts
@@ -1,6 +1,8 @@
 import { useMemo } from "react"
+import { URLSearchParamsInit, useSearchParams } from "react-router-dom"
+import { cleanObject } from "utils"
 import { useProject } from "utils/project"
-import { useUrlQueryParam } from "utils/url"
+import { useSetUrlSearchParams, useUrlQueryParam } from "utils/url"
 
 // 项目列表搜索的参数
 export const useProjectsSearchParams = () =>{
@@ -24,15 +26,17 @@ export const useProjectModal = () => {
         'editingProjectId'
     ])
     const {data: editingProject, isLoading} = useProject(Number(editingProjectId))
+    const setUrlParams = useSetUrlSearchParams()
 
     const open = () => {
         setProjectCreate({projectCreate:true})
     }
     const close = () => {
-        setProjectCreate({projectCreate: undefined})
-        if(editingProjectId){
-            setEditingProjectId({editingProjectId : undefined} )
-        }
+        // setProjectCreate({projectCreate: undefined})
+        // if(editingProjectId){
+        //     setEditingProjectId({editingProjectId : undefined} )
+        // }
+        setUrlParams({projectCreate: '', editingProjectId: ''})
     }
     const startEdit = (id : number) => setEditingProjectId({editingProjectId: id})
 
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,21 +1,41 @@
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { URLSearchParamsInit, useSearchParams } from "react-router-dom"
-import { cleanObject } from "utils"
+import { cleanObject, subset } from "utils"
 
-export const useUrlQueryParam = <K extends string>(keys: K[]) =>{
-    const [searchParams,setSearchParam] = useSearchParams()
+export const useUrlQueryParam = <K extends string>(keys: K[]) => {
+    const [searchParams, setSearchParam] = useSearchParams()
+    const setSearchParams = useSetUrlSearchParams()
+    const [stateKeys] = useState(keys)
     // console.log(searchParams.get('name'));
     return [
-        useMemo(() => keys.reduce((prev, key)=>{
-            return {...prev, [key]: searchParams.get(key) || ''}
-        },{} as {[key in K] : string}),
-        [searchParams]),
-        (params: Partial<{[key in K]: unknown}>) =>{
-            const o = cleanObject({...Object.fromEntries(searchParams), ...params}) as URLSearchParamsInit
-            return setSearchParam(o)
-        } 
+        useMemo(
+            // () => keys.reduce((prev, key) => {
+            //     return { ...prev, [key]: searchParams.get(key) || '' }
+            // }, {} as { [key in K]: string }),
+            // [searchParams]
+            () => 
+            subset(Object.fromEntries(searchParams), stateKeys) as {
+                [key in K]: string
+            },
+            [searchParams, stateKeys]
+        ),
+        (params: Partial<{ [key in K]: unknown }>) => {
+            return setSearchParams(params)
+        }
     ] as const
 }
 
+
+export const useSetUrlSearchParams = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    return (params: { [key in string]: unknown }) => {
+        const o = cleanObject({
+            ...Object.fromEntries(searchParams),
+            ...params,
+        }) as URLSearchParamsInit
+        return setSearchParams(o)
+    }
+}
 
 // const a = ['jack', 12, {gender:'male'}] as const
